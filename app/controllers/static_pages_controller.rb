1acb7aee6e5690835d0300753dc80e4a3b466fe0
Trollegy <kissmelickyour@gmail.com>
2019-08-25 18:29:30 +0300

    Slim,Rubocop and slim-lint

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,9 @@
+# frozen_string_literal: true
+
 class StaticPagesController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def about
-  end
+  def about; end
 
-  def help
-  end
+  def help; end
 end
